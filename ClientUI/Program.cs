e9e33f5243c6293515096eb3d2262767649ef3dd
Mehdi Payervand <mehdipayervand@gmail.com>
2018-07-09 18:43:33 +0430

    SetUp NServiceBus in ClientUI console application

--- ClientUI/Program.cs
+++ ClientUI/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NServiceBus;
 
 namespace ClientUI
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+
+        static async Task AsyncMain()
+        {
+            Console.Title = "ClientUI";
+
+            var endpointConfiguration = new EndpointConfiguration("ClientUI");
+
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
         }
     }
 }
