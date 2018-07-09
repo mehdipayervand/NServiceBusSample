9552da14be765a365098b4dfc1646070c4e8dd0c
Mehdi Payervand <mehdipayervand@gmail.com>
2018-07-09 22:19:58 +0430

    Added Sending the Command

--- ClientUI/Program.cs
+++ ClientUI/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Messages;
 using NServiceBus;
+using NServiceBus.Logging;
 
 namespace ClientUI
 {
@@ -13,8 +15,7 @@ namespace ClientUI
         {
             AsyncMain().GetAwaiter().GetResult();
         }
-
-
+        
         static async Task AsyncMain()
         {
             Console.Title = "ClientUI";
@@ -26,11 +27,50 @@ namespace ClientUI
             var endpointInstance = await Endpoint.Start(endpointConfiguration)
                 .ConfigureAwait(false);
 
-            Console.WriteLine("Press Enter to exit...");
-            Console.ReadLine();
+            //Console.WriteLine("Press Enter to exit...");
+            //Console.ReadLine();
 
+            await RunLoop(endpointInstance)
+                .ConfigureAwait(false);
+            
             await endpointInstance.Stop()
                 .ConfigureAwait(false);
+        }
+
+        static ILog log = LogManager.GetLogger<Program>();
+
+        static async Task RunLoop(IEndpointInstance endpointInstance)
+        {
+            while (true)
+            {
+                log.Info("Press 'P' to place an order, or 'Q' to quit.");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.P:
+                        // Instantiate the command
+                        var command = new PlaceOrder
+                        {
+                            OrderId = Guid.NewGuid().ToString()
+                        };
+
+                        // Send the command to the local endpoint
+                        log.Info($"Sending PlaceOrder command, OrderId = {command.OrderId}");
+                        await endpointInstance.SendLocal(command)
+                            .ConfigureAwait(false);
+
+                        break;
+
+                    case ConsoleKey.Q:
+                        return;
+
+                    default:
+                        log.Info("Unknown input. Please try again.");
+                        break;
+                }
+            }
         }
     }
 }
